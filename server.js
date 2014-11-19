5191f69b404e6d5d1ad56f31458ecf011f6fb58b
Siva <sivasankar.qmax@gmail.com>
2014-11-19 19:30:32 +0530

    add new table

--- server.js
+++ server.js
@@ -33,14 +33,19 @@ var SampleApp = function() {
         };
         var db = mongo.db("mongodb://" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" + process.env.OPENSHIFT_MONGODB_DB_PORT + "/", 
                           {username:process.env.OPENSHIFT_MONGODB_DB_USERNAME,password:process.env.OPENSHIFT_MONGODB_DB_PASSWORD,native_parser:true});
-        db.collection('contactspro').insert({name:"David", title:"About MongoDB"}, function(err, doc){
+        /*db.collection('contactspro').insert({name:"David", title:"About MongoDB"}, function(err, doc){
             if (err) {
                 console.dir(err);
                 return;
             }
             console.log("Contacts Created!!!");
         });
-    };
+    };*/
+db.open(function(err, db) {
+  var collection = db.collection("simple_document_insert_collection_no_safe");
+  // Insert a single document
+  collection.insert({hello:'world_no_safe'});
+}}
 
 
     /**
