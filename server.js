3bca1075a25376726af2eef12b1cce09ec36ad94
Mani Chandrasekar <mani@mobilitas.co>
2014-11-20 16:28:31 +0530

    add mongodb dependency

--- server.js
+++ server.js
@@ -33,19 +33,19 @@ self.setupVariables = function()
         self.connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
         // if OPENSHIFT env variables are present, use the available connection info:
         if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD)
-             {
-          self.connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-          process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-          process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-          process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-          process.env.OPENSHIFT_APP_NAME;
-            }
+        {
+            self.connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+                                     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                                     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                                     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                                     process.env.OPENSHIFT_APP_NAME;
+        }
 
         if (typeof self.ipaddress === "undefined")
-          {
+        {
            
             self.ipaddress = "127.0.0.1";
-          };
+        }
     };
   self.initializeDB = function() 
        {
@@ -59,7 +59,7 @@ self.setupVariables = function()
           {
             if (err)
             {
-                console.dir(err);
+                console.dir(err); 
                 return;
             }
  	
