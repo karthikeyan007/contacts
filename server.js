9e0ad4880db88a10ea2e6f0c09777a654e83fbf3
Siva <sivasankar.qmax@gmail.com>
2014-11-22 15:37:26 +0530

    22-11

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ self.setupVariables = function()
             res.send(self.cache_get('index.html') );
         };
 
-        self.routes['/register'] = self.get('/register',function(req,res) {
+        self.routes['/register'] = self.app.get('/register',function(req,res) {
            
             //var r=req.url;
             res.write("s1"+req.url);
