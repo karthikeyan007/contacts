92f4d56e734adb0b40cd03f66529a90565d9ec51
Siva <sivasankar.qmax@gmail.com>
2014-11-24 19:30:49 +0530

    24-11-14

--- server.js
+++ server.js
@@ -222,7 +222,6 @@ self.setupVariables = function()
                     {
                     if(err) throw err;
                     self.db = db;
-
                     self.db.collection('contactspro').insert({"name":name, "title":ph_no},
                     function(err, doc)
                     { 
