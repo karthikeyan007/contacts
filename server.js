d02dbe87111f7b4161543cc1949b68603795039e
Siva <sivasankar.qmax@gmail.com>
2014-11-24 12:53:14 +0530

    24-11-14

--- server.js
+++ server.js
@@ -174,21 +174,21 @@ self.setupVariables = function()
         for (var r in self.routes) {
             self.app.get(r, self.routes[r]);
         }
-        self.app.post('/regi',function(req,data)
+        self.app.post('/regi',function(req,res)
         { 
 
              var a='hai';
+             var b='';
              res.write(a);
-            res.write(data);
+            
            // res.write(a);
-            /*res.on('data',
+            res.on('data',
                 function(data)
                 {
-                    res.write("data");
-               //   a+=data;  
-                  res.write("a"+a);
-
-                });*/
+                   
+                 b+=data;  
+                  
+                });
             res.on('end',
                 function()
                 {
