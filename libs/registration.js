0fd39501dca8d282057f029c2b8e1f9f0786177e
Siva <sivasankar.qmax@gmail.com>
2014-11-26 10:12:35 +0530

    console undefined in registration

--- libs/registration.js
+++ libs/registration.js
@@ -4,7 +4,7 @@ var database = require('../controllers/database.js');
 exports.userRegistration = function(Server, data, res) {
     console.log("data"+data);
     console.log("Server"+Server);
-    if (data === undefined) {
+    if (data ==='undefined') {
         console.error('Registration information not supplied ' + data);
         res.status(500).send('Invalid request sent. '  +
                              'Please check your JSON data and try again');
